0ac1d7f55a948dfe3bb994091c90b36af50630ac
speechless42 <94534629+speechless42@users.noreply.github.com>
2025-06-06 16:08:23 +0800

    Site updated: 2025-06-06 16:08:22

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1749195126705";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1749197295382";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
